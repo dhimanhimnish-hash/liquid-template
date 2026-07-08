e743f1b3eb362f6c9ffacdd3dc03d889a4553c22
Himnish <himnish@Himnishs-MacBook-Air.local>
2026-07-08 15:02:30 +0530

    Remove Filters, clean up project

--- LiquidTemplates.lean
+++ LiquidTemplates.lean
@@ -1,7 +1,6 @@
 import LiquidTemplates.Basic
 import LiquidTemplates.Proofs
 import LiquidTemplates.Security
-import LiquidTemplates.Filters
 import LiquidTemplates.Injection
 import LiquidTemplates.Json
 import LiquidTemplates.Equivalence
@@ -12,6 +11,5 @@ open Liquid
 #eval eval (Template.seq (Template.text "Hello, ") (Template.var "name")) [("name", Value.str "Alice")]
 #eval eval (Template.ifBlock "show" (Template.text "Visible!") (Template.text "Hidden!")) [("show", Value.bool true)]
 #eval eval (Template.forLoop "item" "items" (Template.seq (Template.var "item") (Template.text ", "))) [("items", Value.list [Value.str "apple", Value.str "banana", Value.str "cherry"])]
-#eval applyFilter "upcase" "hello world"
 #eval escapeStr "<script>alert('xss')</script>"
 #eval toJsonString "hello world"
